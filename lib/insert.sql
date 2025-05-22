86fe765c8a5e439e6feaba9b5f0af6502769f389
Golda Atemba <goldaatemba610@gmail.com>
2025-05-22 09:14:48 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 4, 'M', 'black', 'aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 6, 'F', 'golden', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 10, 'M', 'dark brown', 'serious', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 5, 'F', 'white', 'gentle', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 7, 'M', 'gray', 'grumpy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 3, 'F', 'black', 'playful', 1);
+
+-- unnamed bear
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 2, 'F', 'brown', 'shy', 1);
